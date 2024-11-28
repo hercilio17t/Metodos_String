e88e2e0dac02d889d1882e0f1d1002f075c3f90b
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-28 11:11:09 -0300

    Exercício 4

--- src/exercicio-04/numeroValido.ts
+++ src/exercicio-04/numeroValido.ts
@@ -1,5 +1,28 @@
+/*
+Problema
+Você está desenvolvendo a funcionalidade que verificará se o número digitado pelo usuário no cadastro do seu endereço é válido.
+
+Entrada
+A entrada será composta por uma variável chamada endereco, do tipo string, contendo o endereço do usuário. Essa variável tem a seguinte forma:
+
+NOME_DA_RUA, NUMERO_DA_CASA, CIDADE, ESTADO
+Saída
+Seu programa deve RETORNAR:
+
+true: se o número digitado for válido
+false: se o número digitado for não válidoz
+*/
+
 function solucao(endereco: string): boolean {
   // seu código aqui
+  const enderecoArray: string[] = endereco.split(", ")
+  const numeroConvertido = Number(enderecoArray[1])
+  const verificaNumero = isNaN(numeroConvertido)
+
+  if(verificaNumero){
+    return false
+  }
+  return true
 }
 
 export default solucao;
