378bcbf63a32dd581a6d85673793a83502aaf9a4
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-28 10:27:34 -0300

    Initial commit

---
+++ src/exercicio-01/emailValido.ts
@@ -0,0 +1,25 @@
+/*
+Problema
+Você está desenvolvendo a funcionalidade que verificará se o e-mail digitado pelo usuário no cadastro dos seus dados pessoais é válido.
+
+Para ser um e-mail válido, ele deve conter um caracter "@".
+
+Entrada
+A entrada será composta por uma variável chamada email, do tipo string, contendo o e-mail do usuário.
+
+Saída
+Seu programa deve RETORNAR:
+
+VALIDO: se o e-mail digitado for válido
+INVALIDO: se o e-mail digitado for não válido
+*/
+
+function solucao(email: string): string {
+  // seu código aqui
+  const temArroba: boolean = email.includes('@')
+  if(temArroba){
+    return 'VALIDO'
+  }
+  return 'INVALIDO'
+}
+export default solucao;
